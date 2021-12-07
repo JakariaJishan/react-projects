3745b70543b7e22734d14568842d83c68caf971f
JakariaJishan <jakariajishan987@gmail.com>
2021-12-07 11:37:18 +0600

    done

--- users-comment-web/src/App.js
+++ users-comment-web/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Error from "./components/Error/Error";
 import Home from "./components/Home/Home";
 import Post from "./components/Post/Post";
 
@@ -10,6 +11,7 @@ function App() {
 
       <Routes >
         <Route exact path="/" element={<Home />} />
+        <Route  path="*" element={<Error />} />
         <Route path="/post/:userId" element={<Post/>}/>
       </Routes>
     </BrowserRouter>
---
+++ users-comment-web/src/components/Error/Error.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div>
+            <h1 style={{fontSize: '80px', textAlign:'center'}}>Oops!!!</h1>
+           <p style={{fontSize: '60px', textAlign:'center'}}>404! Page not found</p>
+           
+        </div>
+    );
+};
+
+export default Error;
