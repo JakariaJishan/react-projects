ed2475a59593022934b3c3977980bfb6867a99b9
JakariaJishan <jakariajishan987@gmail.com>
2021-12-22 12:38:37 +0600

    shopping cart

--- react-demo-project-1/src/App.js
+++ react-demo-project-1/src/App.js
@@ -1,11 +1,20 @@
+import {
+  BrowserRouter, Route, Routes
+} from "react-router-dom";
 import Home from "./components/Home/Home";
+import ShowFood from "./components/ShowFood/ShowFood";
+
 
 function App() {
   return (
-    <div className="App">
-      
-      <Home></Home>
+    <BrowserRouter>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/showfood/:foodId" element={<ShowFood />} />
+      </Routes>
     </div>
+  </BrowserRouter>
   );
 }
 
--- react-demo-project-1/src/components/Home/Home.js
+++ react-demo-project-1/src/components/Home/Home.js
@@ -1,20 +1,11 @@
-import React from "react";
-import { data } from "../../Data";
-import FoodBar from "./FoodBar/FoodBar";
-import './FoodBar/FoodBar.css';
-import Header from "./Header/Header";
+import React from 'react';
+
 const Home = () => {
-  return (
-    <div>
-      <Header></Header>
-      <div className="food">
-      {data.map((ele) => (
-        <FoodBar key={ele.id} food={ele}></FoodBar>
-      ))}
-      </div>
-      
-    </div>
-  );
+    return (
+        <div>
+            <h1>hello world</h1>
+        </div>
+    );
 };
 
 export default Home;
