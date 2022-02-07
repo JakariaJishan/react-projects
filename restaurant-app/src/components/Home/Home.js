3b2cc4551baa8012d0a0f4819609f49df0bd5948
JakariaJishan <jakariajishan987@gmail.com>
2022-02-07 13:10:24 +0600

    adding expand card before footer

--- restaurant-app/src/components/Home/Home.js
+++ restaurant-app/src/components/Home/Home.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Contain from '../Container/Contain';
 import Products from '../Products/Products';
+import RecipeReviewCard from '../ReceipeCardReview/ReceipeCardReview';
 
 const Home = () => {
     return (
-        <div>
+        <div style={{
+            width:'80%',
+            margin:'auto'
+        }}>
             <Contain></Contain>
             <Products></Products>
+            <RecipeReviewCard/>
         </div>
     );
 };
